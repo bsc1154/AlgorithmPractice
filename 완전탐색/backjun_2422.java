df6fcb1fe0a347756acdab1961e9e66179dd1c9c
bsc1154 <khl1154@naver.com>
2022-11-20 10:51:32 +0900

    Update backjun_2422.java

--- 완전탐색/backjun_2422.java
+++ 완전탐색/backjun_2422.java
@@ -1,42 +1,40 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.StringTokenizer;
 public class Main {
-	static int N, M, map[][], count = 0;
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine());
-		N = stoi(st.nextToken());
-		M = stoi(st.nextToken());
-		boolean visit[] = new boolean[N + 1];
-		map = new int[N + 1][N + 1];
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(in.readLine());
-			int u = stoi(st.nextToken());
-			int v = stoi(st.nextToken());
-			map[u][v] = 1;
-			map[v][u] = 1;
-		}
-		dfs(1, 0, new int[3]);
-		System.out.println(count);
-	}
+        try {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            boolean[][] check = new boolean[200][200];
 
-	static void dfs(int val, int selidx, int sel[]) {
-		if(selidx == 3) {
-			if(map[sel[0]][sel[1]]==1 || map[sel[1]][sel[2]]==1 || map[sel[0]][sel[2]]==1) 
-				return;
-			count++;
-			return;
-		}
-		if(val == N+1)
-			return;
-		sel[selidx] = val;
-		dfs(val+1, selidx+1, sel);
-		dfs(val+1, selidx, sel);
-	}
+            for(int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int ice1 = Integer.parseInt(st.nextToken());
+                int ice2 = Integer.parseInt(st.nextToken());
+                check[ice1 - 1][ice2 - 1] = true;
+                check[ice2 - 1][ice1 - 1] = true;
+            }
 
-	static int stoi(String s) {
-		return Integer.valueOf(s);
-	}
+            int ans = 0;
+            for(int i = 0; i < N; i++) {
+                for(int j = i + 1; j < N; j++) {
+                    for(int k = j + 1; k < N; k++) {
+                        if(check[i][j] || check[i][k] || check[j][k])
+                            continue;
+                        ans++;
+                    }
+                }
+            }
+
+            System.out.println(ans);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
